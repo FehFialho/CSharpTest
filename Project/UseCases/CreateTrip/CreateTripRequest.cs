2b294736935522a55d416f52bc3255838b047038
FehFialho <fernandaklechowicz@gmail.com>
2025-09-10 15:22:09 -0300

    Update Requirements for CreateTrip

--- Project/UseCases/CreateTrip/CreateTripRequest.cs
+++ Project/UseCases/CreateTrip/CreateTripRequest.cs
@@ -1,6 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Project.UseCases.CreateTrip;
 
-public record CreateTripRequest(
-    string title,
-    string description
-);
+public record CreateTripRequest
+{
+    [MaxLength(20)]
+    public string title { get; set; }
+
+    [MinLength(40)]
+    [MaxLength(200)]
+    public string description { get; set; }
+}
